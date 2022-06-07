b901c5f8c67f680f325000cd3f417b72afb8b231
Asandei Stefan <stefan.asandei@protonmail.com>
2022-06-07 22:18:17 +0300

    refactor

---
+++ src/hittable.go
@@ -0,0 +1,21 @@
+package main
+
+type HitRecord struct {
+	Intersec Point3
+	Normal Vector3
+	T float64
+	IsFrontFace bool
+}
+
+func (rec *HitRecord) SetFaceNormal(r *Ray, outward_normal Vector3) {
+	rec.IsFrontFace = dot(r.Direction, outward_normal) < 0
+	if rec.IsFrontFace {
+		rec.Normal = outward_normal
+	} else {
+		rec.Normal = outward_normal.Multiply(-1)
+	}
+}
+
+type Hittable interface {
+	hit(ray *Ray, t_min float64, t_max float64, rec *HitRecord) bool
+}
---
+++ src/sphere.go
@@ -0,0 +1,38 @@
+package main
+
+import "math"
+
+type Sphere struct {
+	Center Point3
+	Radius float64
+}
+
+func (sphere *Sphere) hit(ray *Ray, t_min float64, t_max float64, rec *HitRecord) bool {
+	oc := vectors_substract(ray.Origin, sphere.Center)
+
+    a := ray.Direction.LengthSquared()
+    half_b := dot(oc, ray.Direction)
+    c := oc.LengthSquared() - sphere.Radius * sphere.Radius
+
+    discriminant := half_b*half_b - a*c
+
+	if discriminant < 0 {
+        return false
+    }
+
+	sqrtd := math.Sqrt(discriminant)
+	root := (-half_b-sqrtd)/a
+	if root < t_min || t_max < root {
+		root = (-half_b+sqrtd)/a
+		if root < t_min || t_max < root {
+			return false
+		}
+	}
+
+	rec.T = root
+	rec.Intersec = point_at(ray, root)
+	outward_normal := vectors_substract(rec.Intersec, sphere.Center).Divide(sphere.Radius)
+	rec.SetFaceNormal(ray, outward_normal)
+
+	return true
+}
